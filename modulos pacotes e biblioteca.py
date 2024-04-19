ef5f06d345e2616d22dd5d2fed298ead70389e6d
tiago abdalla <75739193+tiagoabdalla2@users.noreply.github.com>
2024-04-19 08:51:12 -0300

    Add files via upload

---
+++ modulos pacotes e biblioteca.py
@@ -0,0 +1,46 @@
+"""
+modulos , pacotes e bibliotecas em python
+"""
+import time
+import random as rnd
+from random import randint 
+
+# NÃO USAR!!!!!!!
+# from random import *
+
+
+
+# def randint(x):
+#     print(x)
+
+print (rnd,randint(1,6))
+print(randint(1,6))
+
+texto = input()
+print(texto,())
+"""
+re regular expression serve pra montar uma expresao regular 
+datetime bom importe pra lidar com datas 
+funçao e1 lida com cor 
+math 
+sqlite3 banco de dados 
+"""
+lista = [ 1, 2 , 3 , 4 , 5 , 6]
+rnd.shuffle(outra)
+
+
+
+def fatorial(n):
+    fat = 1 
+    for 1 in range(1, n + 1 ):
+        fat *= 1 
+        
+    return fat 
+inicio= time.time()
+fatorial(100000)
+print(F"tempo de corrido:" (time.time() - inicio:.2 F ) "segundos.")
+
+"""
+como estalar uma biblioteca externa 
+usar no terminal usar pip e o nome da biblioteca 
+"""
